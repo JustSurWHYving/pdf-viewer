6c0d9d8348a5b01d190eb8029b5d61f1a5475b0e
Ashish Prajapati <ashishanshprajapati@gmail.com>
2024-08-11 02:02:14 +0530

    broken parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-use std::fs;
+use pdf::file::File as PdfFile;
+use pdf::object::{Object, ObjectStream};
+use pdf::parser::parse_stream as Parser;
+use std::fs::File;
+use std::io::BufReader;
+
+fn load_pdf(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+
+    let file = File::open(file_path)?;
+    let reader = BufReader::new(file);
+
+    let pdf_file = PdfFile::from_reader(reader)?;
+
+    let mut parser = Parser::new(&pdf_file);
+
+    while let Some(object) = parser.next_object()? {
+        match object {
+            ObjectStream::Page(page) => {
+                println!("Page: {:?}", page);
+            }
+            _ => {}
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
-    let file_path = "test.pdf";
-    // --snip--
-    println!("In file {file_path}");
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = load_pdf("example.pdf") {
+        eprintln!("Error loading PDF: {}", e);
+    }
 }
